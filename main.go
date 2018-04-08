52506c85169db49740fe8dca9d134059528e9f0d
LittleCurry <2441611062@qq.com>
2018-04-08 15:45:21 +0800

    first main

---
+++ main.go
@@ -0,0 +1,27 @@
+
+package main
+
+import(
+ 	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/login",login)
+	if err := http.ListenAndServe(":80", nil); err !=nil {
+		fmt.Println("ListenAndServe err", err)
+	}
+}
+
+func login(w http.ResponseWriter, r *http.Request){
+	r.ParseForm()
+	fmt.Println(r.Method)
+	if r.Method=="GET"{
+		fmt.Fprintf(w, "This is a GET request")
+	}else{
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	fmt.Println("Recived info:", r.Form)
+	fmt.Fprintf(w, r.Form.Get("info"))
+	}
+
+}
